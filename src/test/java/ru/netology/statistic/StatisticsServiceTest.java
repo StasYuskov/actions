4c6deafc151ea5008002202e227678e4e40c892b
StasYuskov <yuskov.itrum@gmail.com>
2026-02-12 15:19:57 +0300

    add and refactor autotest, increasing the coverage to 100%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,20 +1,31 @@
 package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
-  @Test
-  void findMax() {
+  @ParameterizedTest
+  @MethodSource("findMaxData")
+  void findMax(long[] incomesInBillions, long expected) {
     StatisticsService service = new StatisticsService();
-
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
-
     long actual = service.findMax(incomesInBillions);
 
     assertEquals(expected, actual);
+  }
+
+  static Stream<Arguments> findMaxData() {
+    return Stream.of(
+            Arguments.of(new long[] {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12},
+    12),
+            Arguments.of(new long[] {12, 5, 8, 4, 5, 3, 8, 26, 11, 11, 12},
+                    26)
+    );
   }
 }
